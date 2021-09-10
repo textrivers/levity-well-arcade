b01ca08ad51143ccee542d323c9b6602a635f707
textrivers <jon.p.woodward@gmail.com>
2021-09-10 17:22:51 -0400

    UI and groundwork for game flow

--- Jump Around/Acorn.gd
+++ Jump Around/Acorn.gd
@@ -2,15 +2,5 @@ extends Area2D
 
 signal acorn_collected
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Acorn_body_entered(_body):
 	emit_signal("acorn_collected")
--- Jump Around/UI.gd
+++ Jump Around/UI.gd
@@ -1,31 +1,24 @@
-extends RichTextLabel
+extends Control
 
 var count: int = 0
+var counter
 
 func _ready():
 # warning-ignore:return_value_discarded
 	get_parent().get_node("Hole/Contents/Squirrel").connect("jump", self, "on_squirrel_jump")
 # warning-ignore:return_value_discarded
 	get_parent().get_node("Hole/Contents/Acorn").connect("acorn_collected", self, "on_acorn_collected")
+	counter = $Counter
 
 func on_squirrel_jump():
 	count += 1
-	text = str(count)
+	counter.text = str(count)
 
 func on_acorn_collected():
 	get_tree().set_pause(true)
-	$Button.show()
-
-func _on_RichTextLabel_mouse_entered():
-	$Button.show()
-
-func _on_RichTextLabel_mouse_exited():
-	$Button.hide()
-
-func _on_Button_pressed():
-	get_tree().set_pause(false)
+	## fix this once scene transitions are possible
 # warning-ignore:return_value_discarded
 	get_tree().reload_current_scene()
+	get_tree().set_pause(false)
 
-func _on_Button_mouse_entered():
-	$Button.show()
+
