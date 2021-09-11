aada8a7114c0932fed548cdfac6b0f07ad96ce00
textrivers <jon.p.woodward@gmail.com>
2021-09-11 19:03:56 -0400

    more holes, moving platforms can wrap screen

--- Jump Around/MovingPlatform.gd
+++ Jump Around/MovingPlatform.gd
@@ -5,7 +5,8 @@ var pos_b: Vector2 = Vector2(0, 0)
 export var travel: Vector2 = Vector2(0, 0)
 export var speed: float = 300.0
 var tween
-export var delay = 1.0
+export var delay = 0.0
+export var wrap = false
 
 func _ready():
 	pos_a = global_position
@@ -13,7 +14,8 @@ func _ready():
 	tween = $Tween
 	var duration = pos_a.distance_to(pos_b) / float(speed)
 	tween.interpolate_property(self, "position", pos_a, pos_b, duration, Tween.TRANS_LINEAR, Tween.EASE_IN, delay)
-	tween.interpolate_property(self, "position", pos_b, pos_a, duration, Tween.TRANS_LINEAR, Tween.EASE_IN, (delay + duration) * 2)
+	if wrap == false:
+		tween.interpolate_property(self, "position", pos_b, pos_a, duration, Tween.TRANS_LINEAR, Tween.EASE_IN, duration + (delay * 2))
 	tween.start()
 
 
