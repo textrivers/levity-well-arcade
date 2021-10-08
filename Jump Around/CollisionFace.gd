546f18cd7de78982bcd1dbe9e42c205f666ceef8
textrivers <jon.p.woodward@gmail.com>
2021-10-08 13:09:07 -0400

    creating collision from mesh data
    Pretty fast! Now just need to update mesh data (by removing vertices) and see what happens to the texture. Presumably any part of the texture that's outside of the mesh won't render.

--- Jump Around/CollisionFace.gd
+++ Jump Around/CollisionFace.gd
@@ -14,6 +14,8 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("collidify"):
 		_create_collision_polygon()
+	if Input.is_action_just_pressed("meshify"):
+		_create_collision_mesh()
 
 func _create_collision_polygon():
 	var startclick = OS.get_ticks_usec()
@@ -31,10 +33,32 @@ func _create_collision_polygon():
 		for j in range(poly.size() - 1, 0, -2):
 			poly.remove(j)
 		print(poly.size())
-		my_collision.set_polygon(my_polygon.polygons[i])
+		my_collision.set_polygon(poly)
 		my_collision.scale = scale
 		my_collision.position.x -= tex_width / 2
 		my_collision.position.y -= tex_height / 2
 		call_deferred("add_child", my_collision)
 	var endclick = OS.get_ticks_usec()
-	print(float(endclick - startclick) / 100000)
+	print(float(endclick - startclick) / 1000000)
+
+func _create_collision_mesh():
+	var startclick = OS.get_ticks_usec()
+	var verts = $Sprite2.mesh.surface_get_arrays(0)
+	verts = verts[0]
+	var my_polygon = Polygon2D.new()
+	my_polygon.set_polygons(verts)
+	print(my_polygon.polygons.size())
+	#for i in range(my_polygon.polygons.size()):
+	var my_collision = CollisionPolygon2D.new()
+		#poly = my_polygon.polygons[i]
+		#print(poly.size())
+		#for j in range(poly.size() - 1, 0, -2):
+		#	poly.remove(j)
+		#print(poly.size())
+	my_collision.set_polygon(my_polygon.polygons)
+	my_collision.scale = scale
+		#my_collision.position.x -= tex_width / 2
+		#my_collision.position.y -= tex_height / 2
+	call_deferred("add_child", my_collision)
+	var endclick = OS.get_ticks_usec()
+	print(float(endclick - startclick) / 1000000)
