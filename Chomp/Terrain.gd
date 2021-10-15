198b1079edfae11c85894330dae21502c4422797
textrivers <jon.p.woodward@gmail.com>
2021-10-15 16:40:45 -0400

    destructible terrain
    expand on this for maximum chompability

--- Chomp/Terrain.gd
+++ Chomp/Terrain.gd
@@ -7,6 +7,7 @@ var default_sq = [
 	Vector2(-10, 10)
 ]
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var match_poly = $Collision.polygon
@@ -15,8 +16,34 @@ func _ready():
 	$Collision/Visible.texture_offset.y += ($Collision/Visible.texture.get_height() / 2)
 
 func _input(event):
-	if Input.is_action_just_pressed("leftclick"):
-		pass
-		## get click location
-		## carve it out of existing polygon
-		## set the visible polygon to match
+	if Input.is_action_pressed("leftclick"):
+		var click_pos = get_global_mouse_position()
+		carve_polygons(click_pos)
+	if Input.is_action_pressed("rightclick"):
+		var smallball = load("res://SmallBall.tscn").instance()
+		var click_pos = get_global_mouse_position()
+		add_child(smallball)
+		smallball.position = click_pos
+
+func carve_polygons(click):
+	var new_poly: PoolVector2Array = []
+	for point in default_sq:
+		new_poly.append(point + click)
+	var result = Geometry.clip_polygons_2d($Collision/Visible.polygon, new_poly)
+	if result.size() == 0:
+		return
+	else:
+		for res in range(result.size()):
+			if res == 0:
+				$Collision/Visible.polygon = result[0]
+				$Collision.set_deferred("polygon", result[0])
+			if res > 0:
+				var new_col = CollisionPolygon2D.new()
+				var new_vis = Polygon2D.new()
+				new_vis.polygon = result[res]
+				new_col.polygon = result[res]
+				new_vis.texture = $Collision/Visible.texture
+				var new_rig = RigidBody2D.new()
+				add_child(new_rig)
+				new_rig.add_child(new_col)
+				new_col.add_child(new_vis)
